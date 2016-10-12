2c4994004b6d80ac34a986e6fec72148161243a9
banyh <banyhong@gliacloud.com>
2016-10-12 03:53:36 +0000

    enable multi-language

--- ParserJPype.java
+++ ParserJPype.java
@@ -27,15 +27,28 @@ public class ParserJPype {
     private LexicalizedParser lp = null;
     private GrammaticalStructureFactory gsf = null;
 
-    public void init() throws Exception {
+    public void init(String lang) throws Exception {
         System.setOut(new PrintStream(System.out, true, "utf-8"));
-        lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/chineseFactored.ser.gz");
+        String model_path = "edu/stanford/nlp/models/lexparser/";
+        String model;
+        if (lang.equals("zh")) {
+            model = "chineseFactored.ser.gz";
+        } else if (lang.equals("en")) {
+            model = "englishFactored.ser.gz";
+        } else if (lang.equals("fr")) {
+            model = "frenchFactored.ser.gz";
+        } else if (lang.equals("de")) {
+            model = "germanFactored.ser.gz";
+        } else {
+            throw new Exception("Wrong language parameter: " + lang);
+        }
+        lp = LexicalizedParser.loadModel(model_path + model);
         gsf = lp.treebankLanguagePack().grammaticalStructureFactory();
     }
 
     public String getParserResult(String text) throws Exception {
         if (gsf == null) {
-            init();
+            init("zh");
         }
         // String text = "俄国 希望 伊朗 没有 制造 核武器 计划 。$$$俄国 希望 伊朗 没有 制造 核武器 计划 。##NR VV NR AD VV NN NN PU$$$NR VV NR AD VV NN NN PU";
         String[] text_tag = text.split("###");
